5dc475c3b71108174634e4e121ea8c37e1fa8644
Lavínia <lavizsenai@gmail.com>
2026-04-09 09:09:36 -0300

    finalizado

---
+++ api-ecommerce/src/main/java/br/com/senai/api_ecommerce/categoria/DadosListagemCategoria.java
@@ -0,0 +1,7 @@
+package br.com.senai.api_ecommerce.categoria;
+
+public record DadosListagemCategoria(Long id, String nome) {
+    public DadosListagemCategoria(Categoria categoria){
+        this(categoria.getId(),  categoria.getNome());
+    }
+}
--- api-ecommerce/src/main/java/br/com/senai/api_ecommerce/controller/CategoriaController.java
+++ api-ecommerce/src/main/java/br/com/senai/api_ecommerce/controller/CategoriaController.java
@@ -1,12 +1,15 @@
 package br.com.senai.api_ecommerce.controller;
 
-import br.com.senai.api_ecommerce.categoria.Categoria;
-import br.com.senai.api_ecommerce.categoria.CategoriaRepository;
-import br.com.senai.api_ecommerce.categoria.DadosCadastroCategoria;
+import br.com.senai.api_ecommerce.categoria.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -24,7 +27,38 @@ public class CategoriaController {
     }
 
     @GetMapping
-    public List<Categoria> listarCategorias(){
-        return repository.findAll();
+    public Page<DadosListagemCategoria> listarCategorias(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao)
+                .map(DadosListagemCategoria::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizarCategoria(@RequestBody @Valid DadosAtualizarCategoria dados){
+        var categoria = repository.getReferenceById(dados.id());
+        categoria.atualizarCategoria(dados);
+    }
+
+//    @DeleteMapping("/{id}")
+//    @Transactional
+//    public void deletarCategoria(@PathVariable Long id){
+//        repository.deleteById(id);
+//    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void deletarCategoria(@PathVariable Long id){
+        var categoria = repository.getReferenceById(id);
+        categoria.excluirCategoria();
+    }
+
+    @GetMapping("/{id}")
+    public DadosDetalhamentoCategoria detalharCategoria(@PathVariable Long id){
+        Categoria categoria = repository.findByIdAndAtivoTrue(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Categoria não encontrada"
+                ));
+        return new DadosDetalhamentoCategoria(categoria);
     }
 }
